c7be73029bc9ec3bdae4843c19527c8e1e0bb0da
Maia <maia.mcc@gmail.com>
2020-04-22 18:31:15 -0400

    restart_process: try start.sh instead of entr

--- restart_process/release.sh
+++ restart_process/release.sh
@@ -3,14 +3,14 @@
 set -e
 
 TIMESTAMP=$(date +'%Y-%d-%m')
-IMAGE_NAME='tiltdev/entr'
+IMAGE_NAME='tiltdev/restart-scripts'
 IMAGE_WITH_TAG=$IMAGE_NAME:$TIMESTAMP
 
-# build Docker image with our statically compiled entr binary for Linux
+# build Docker image with our start and restart scripts from latest master
 docker build . -t $IMAGE_NAME
 docker push $IMAGE_NAME
 
-docker tag tiltdev/entr $IMAGE_WITH_TAG
+docker tag $IMAGE_NAME $IMAGE_WITH_TAG
 docker push $IMAGE_WITH_TAG
 
 
