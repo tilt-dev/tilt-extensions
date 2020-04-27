6262e91ca56b37f0e9fdcef52cfbdff56d8a725b
Maia McCormick <maia@windmill.engineering>
2020-04-27 16:59:42 -0400

    restart_process: use pre-compiled bin of entr that doesn't need to be run as shell (#13)

--- restart_process/release.sh
+++ restart_process/release.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-TIMESTAMP=$(date +'%Y-%d-%m')
+TIMESTAMP=$(date +'%Y-%m-%d')
 IMAGE_NAME='tiltdev/entr'
 IMAGE_WITH_TAG=$IMAGE_NAME:$TIMESTAMP
+
+# build our binary
+env GOOS=linux GOARCH=amd64 go build tilt-restart-wrapper.go
 
 # build Docker image with our statically compiled entr binary for Linux
 docker build . -t $IMAGE_NAME
@@ -12,6 +15,8 @@ docker push $IMAGE_NAME
 
 docker tag tiltdev/entr $IMAGE_WITH_TAG
 docker push $IMAGE_WITH_TAG
+
+echo "Successfully built and pushed $IMAGE_WITH_TAG"
 
 
 
---
+++ restart_process/tilt-restart-wrapper.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+)
+
+var watchFile = flag.String("watch_file", "/.restart-proc", "File that entr will watch for changes; changes to this file trigger `entr` to rerun the command(s) passed")
+var entrPath = flag.String("entr_path", "/entr", "Path to `entr` executable")
+
+func main() {
+	flag.Parse()
+
+	cmd := exec.Command(*entrPath, "-rz")
+	cmd.Stdin = strings.NewReader(fmt.Sprintf("%s\n", *watchFile))
+	cmd.Args = append(cmd.Args, flag.Args()...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		if exiterr, ok := err.(*exec.ExitError); ok {
+			// The program has exited with an exit code != 0
+			if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+				if len(flag.Args()) == 0 {
+					log.Println("`tilt-restart-wrapper` requires at least one positional arg " +
+						"(a command or set of args to be  executed / rerun whenever `watch_file` changes)")
+				}
+				os.Exit(status.ExitStatus())
+			}
+		} else {
+			log.Fatalf("error running command: %v", err)
+		}
+	}
+
+	fmt.Println("hi there")
+	if len(flag.Args()) == 0 {
+		log.Fatal("`tilt-restart-wrapper` requires at least one positional arg "+
+			"(will be passed to `entr` and executed / rerun whenever `watch_file` changes)")
+	}
+}
