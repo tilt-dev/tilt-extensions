45c7a27c92325b3973ac5e95b7adf1acf3eeb88e
Maia <maia.mcc@gmail.com>
2020-12-07 16:24:49 -0500

    better rsync bin path

--- syncback/krsync.sh
+++ syncback/krsync.sh
@@ -3,9 +3,11 @@
 # Adapted from Karl Bunch's post on Server Fault:
 # https://serverfault.com/questions/741670/rsync-files-to-a-kubernetes-pod/887402#887402
 
+rsync_path=/bin/rsync.tilt
+
 if [ -z "$KRSYNC_STARTED" ]; then
   export KRSYNC_STARTED=true
-  exec rsync --rsync-path=/app/rsync.tilt --blocking-io --rsh "$0" $@
+  exec rsync --rsync-path=$rsync_path --blocking-io --rsh "$0" $@
 fi
 
 # Running as --rsh
@@ -24,7 +26,7 @@ fi
 # ensure our rsync bin is present on container in the expected location
 ## TODO: option for windows bin, depending on container OS
 tar_path=${0%/*}/rsync.tilt.tar
-cat $tar_path | kubectl $namespace exec -i deployment/$depl -- /bin/sh -c 'ls rsync.tilt 2>/dev/null && cat>/dev/null || tar -xf -' > /dev/null
+cat $tar_path | kubectl $namespace exec -i deployment/$depl -- /bin/sh -c "ls $rsync_path 2>/dev/null && cat>/dev/null || mkdir /tmp/rsync && tar -xf - -C /bin/" > /dev/null
 
 exec kubectl $namespace exec -i deployment/$depl -- "$@"
 
