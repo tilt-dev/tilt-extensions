0a934b7156801f8797324a8940889232dff5deda
Maia <maia.mcc@gmail.com>
2020-12-08 14:29:24 -0500

    code review

--- syncback/krsync.sh
+++ syncback/krsync.sh
@@ -6,9 +6,10 @@
 set -e
 
 # make sure local tmp dir exists
-[[ -d /tmp/rsync.tilt ]] || mkdir /tmp/rsync.tilt
+mkdir -p /tmp/rsync.tilt
 
 rsync_path=/bin/rsync.tilt
+tar_path_local=${0%/*}/rsync.tilt.tar  # TODO: option for windows binary
 
 if [ -z "$KRSYNC_STARTED" ]; then
   export K8S_OBJECT=$1
@@ -31,9 +32,13 @@ if [ "X$depl" = "X-l" ]; then
 fi
 
 # ensure our rsync bin is present on container in the expected location
-## TODO: option for windows bin, depending on container OS
-tar_path=${0%/*}/rsync.tilt.tar
-kubectl exec deployment/example-rails -- /bin/sh -c "ls $rsync_path > /dev/null" 2> /dev/null || cat $tar_path | kubectl $namespace exec -i $K8S_OBJECT -- /bin/sh -c "tar -xf - -C /bin/"
+set +e # disable exit on error
+kubectl exec $K8S_OBJECT -- ls $rsync_path > /dev/null 2>&1
+exit_code="$?"
+set -e
+if [[ "$exit_code" != "0" ]]; then
+  cat $tar_path_local | kubectl $namespace exec -i $K8S_OBJECT -- tar -xf - -C /bin/
+fi
 
 exec kubectl $namespace exec -i $K8S_OBJECT -- "$@"
 
