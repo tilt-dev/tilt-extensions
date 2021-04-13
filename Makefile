df310182f9adc2b949d6722c1940cb5a61543b5b
Nick Santos <nicholas.j.santos@gmail.com>
2021-04-13 13:11:33 -0400

    Add a podman extension

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-.PHONY: test
+.PHONY: test publish-ci-image
 
 test:
 	./test.sh
+
+publish-ci-image:
+	cd .circleci && docker build -t tiltdev/tilt-extensions-ci .
+	docker push tiltdev/tilt-extensions-ci
--- test.sh
+++ test.sh
@@ -9,6 +9,13 @@ git_resource/test/test.sh
 namespace/test/test.sh
 helm_remote/test/test.sh
 ko/test/test.sh
+
+# TODO(nick): Currently it's a PITA to run podman in CI,
+# so we've turned this off. You can still run it manually
+# on a machine with podman installed.
+#
+# podman/test/test.sh
+
 secret/test/test.sh
 restart_process/test/test.sh
 tests/golang/test/test.sh
