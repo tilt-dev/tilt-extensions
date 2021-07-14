8b60bb7caf1e65f04d7ea6ffee08d08861d89c5d
Lucie le Blanc <lucie@tilt.dev>
2021-07-14 12:41:23 -0400

    find and print blocking dependencies

--- list_dependencies/deps.py
+++ list_dependencies/deps.py
@@ -2,8 +2,20 @@
 import subprocess
 import json
 
+class Resource:
+    def __init__(self, name, status, deps):
+        self.name = name
+        self.status = status
+        self.deps = deps
+
+    def __str__():
+        return str(name) + str(status) + str(deps)
+
 # replicate the following:
+# 1. find dependencies
 # $ tilt dump engine | jq '.ManifestTargets[].Manifest | { name:.Name , deps:.ResourceDependencies } '
+# 2. find status
+# $ tilt dump engine | jq '.ManifestTargets[] | { resource_name:.Manifest.Name, pods:.State.RuntimeState.Pods } | .pods["\(.resource_name)"].containers[].ready'
 
 stdout_str = subprocess.run(['tilt', 'dump', 'engine'], stdout=subprocess.PIPE).stdout.decode('utf-8')
 data = json.loads(stdout_str)["ManifestTargets"]
@@ -11,12 +23,25 @@ data = json.loads(stdout_str)["ManifestTargets"]
 # construct a forward graph given the output
 
 g_f = {}
+node_ready = {}
 
 for resInfo in data.values():
     manifest = resInfo["Manifest"]
     resName = manifest["Name"]
     resDeps = manifest["ResourceDependencies"]
+    if resDeps == None:
+        resDeps = []
     g_f[resName] = resDeps
+
+    pods = resInfo["State"]["RuntimeState"]["Pods"]
+    if resName in pods:
+        resStatus = pods[resName]["containers"][0]["ready"]
+    else:
+        resStatus = False
+    node_ready[resName] = resStatus
+
+print(g_f)
+print(node_ready)
 
 # construct the reverse graph
 
@@ -33,15 +58,23 @@ for (start, neighbors) in g_f.items():
 
 print(g_r)
 
-# now what?
-# find which of these resources are unbuilt.
+# find blocking
 
-# command to replicate:
-# tilt dump engine | jq '.ManifestTargets[] | { resource_name:.Manifest.Name, pods:.State.RuntimeState.Pods } | .pods["\(.resource_name)"].containers[].ready'
+def find_blocking(node):
+    deps = g_f[node]
+    pending = []
+    for i in deps:
+        if not node_ready[i]:
+            pending.append(i)
+    return pending
 
-for resInfo in data.values():
-    resource_name = resInfo["Manifest"]["Name"]
-    pods = resInfo["State"]["RuntimeState"]["Pods"]
-    resStatus = pods[resource_name]["containers"][0]["ready"]
-    print(resStatus)
+def format_list(mylist):
+    return str(mylist)[1:-1]
+
+for node in g_r.keys():
+    blocking = find_blocking(node)
+    if len(blocking) > 0:
+        print("Resource \"" + node + "\" is blocking on " + format_list(blocking))
+    else:
+        print("Resource \"" + node + "\" is not blocked by any dependencies.")
 
