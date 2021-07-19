5ba119ab71d7f47b5c98219ffab5298699f3feff
Lucie le Blanc <lucie@tilt.dev>
2021-07-19 17:21:16 -0400

    execute the command pointed to by index

--- workflow/exec_workflow.sh
+++ workflow/exec_workflow.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-echo "executing command: '$(head -n 1 workflow_cmds.tmp)'"
-$(head -n 1 workflow_cmds.tmp)
+WORKFLOW_INDEX=$(cat workflow_index.tmp | head -n 1)
+echo "executing command $WORKFLOW_INDEX: '$( head -n $WORKFLOW_INDEX workflow_cmds.tmp | tail -n 1 )'"
+$(head -n $WORKFLOW_INDEX workflow_cmds.tmp | tail -n 1)
 
