9ee347afaf45a714dfa6a51a931e2794762a73be
lucieleblanc <lucieleblanc@users.noreply.github.com>
2021-07-29 14:58:33 -0400

    workflow: buttons for a simple linear workflow (#207)
    * set up basic workflow functions in .py form
    
    * extract non-Tiltfile commands into separate functions
    
    * shell scripts for file operations
    
    * execute the command pointed to by index
    
    * correctly increment index file
    
    * bundle commands into main play/replay/rewind scripts
    
    * connect scripts to buttons
    
    * generalize scripts to allow for multiple workflows
    
    * remove python draft script
    
    * omit resource name in button text
    
    * add overview in README
    
    * separate test and extension
    
    * add usage example to clarify work_cmds and clear_cmd types
    
    * reduce output noise on workflow setup
    
    * add quote safeguards on echo/reset commands
    
    * add quoting around filenames in shell script
    
    * fix filepaths for individual steps
    
    * add test scripts
    
    * appease shellcheck
    
    * add executable permissions to CI test script
    
    * display more inforamative message on replay after rewind
    
    * singular step

---
+++ workflow/rewind.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash 
+
+set -eou pipefail
+
+if [ $# -ne 2 ]
+then
+	echo "usage: $0 <workflow_name> <resource_name>"
+	exit 1
+fi
+
+"$(dirname "$0")/write_index.sh" "$@" 0
+"$(dirname "$0")/exec_reset.sh" "$@"
