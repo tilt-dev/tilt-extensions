0e19bd14c6ba318505af655f825c2ae6a04a8ff6
Lucie le Blanc <lucie@tilt.dev>
2021-07-29 12:00:07 -0400

    display more inforamative message on replay after rewind

--- workflow/exec_workflow.sh
+++ workflow/exec_workflow.sh
@@ -15,5 +15,11 @@ indexfile="workflow_index-$resource_name-$workflow_name.tmp"
 cmdsfile="workflow_cmds-$resource_name-$workflow_name.tmp"
 
 WORKFLOW_INDEX=$(head -n 1 <"$indexfile")
+
+if [[ "$WORKFLOW_INDEX" -eq "0" ]] ; then
+	echo "no steps to replay"
+	exit 0
+fi
+
 $(head -n "$WORKFLOW_INDEX" "$cmdsfile" | tail -n 1)
 
