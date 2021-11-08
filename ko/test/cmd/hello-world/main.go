2aecc1b2efbf0a5d9f92dd23c995f786a036962f
Nick Santos <nick@tilt.dev>
2021-11-08 16:38:51 -0500

    ko: flesh out the ko test a bit (#289)

--- ko/test/cmd/hello-world/main.go
+++ ko/test/cmd/hello-world/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func main() {
+	http.Handle("/", http.FileServer(http.Dir(os.Getenv("KO_DATA_PATH"))))
 	http.ListenAndServe(":8080", nil)
 }
