2ec66a41e0a4bdae3f1a1a27e1d78a37fae5e3cc
Nick Sieger <nicksieger@gmail.com>
2021-12-06 17:18:49 -0600

    syncback: krsync.sh uses existing rsync (#278)
    * syncback: krsync.sh uses existing rsync
    
    Change krsync.sh to check for and use an existing rsync binary if one is
    found that is version 3 or greater.
    
    Simplify calling of krsync.sh to pass namespace as part of $K8S_OBJECT
    first argument to the script.
    
    * syncback: fix: use $rsync_tilt_path here
    
    * syncback: don't quote $K8S_OBJECT
    
    This allows the value to be interpreted as multiple arguments when
    namespace and/or container args are supplied.
    
    * Add syncback test
    
    * Add test for container that already has rsync installed
    
    * Clarify $K8S_OBJECT setting and usage

--- syncback/krsync.sh
+++ syncback/krsync.sh
@@ -5,21 +5,54 @@
 
 set -e
 
-# make sure local tmp dir exists
+# make sure local tmp dir exists (see -T= argument in Tiltfile)
 mkdir -p /tmp/rsync.tilt
 
-rsync_path=/bin/rsync.tilt
-tar_path_local=${0%/*}/rsync.tilt.tar  # TODO: option for windows binary
+rsync_tilt_path=/bin/rsync.tilt
+
+install_rsync_tilt() {
+    if [ -z "$RSYNC_PATH" ]; then
+        # rsync already checked/installed
+        return 0
+    fi
+
+    local tar_path_local=${0%/*}/rsync.tilt.tar  # TODO: option for windows binary
+    # ensure our rsync bin is present on container in the expected location
+    set +e # disable exit on error
+    # $K8S_OBJECT is intentionally not quoted here so -n/-c flags can be
+    # parsed by kubectl
+    kubectl exec $K8S_OBJECT -- ls "$rsync_tilt_path" > /dev/null 2>&1
+    exit_code="$?"
+    set -e
+    if [[ "$exit_code" != "0" ]]; then
+        # shellcheck disable=SC2002
+        cat "$tar_path_local" | kubectl exec -i $K8S_OBJECT -- tar -xf - -C $(dirname $rsync_tilt_path)
+    fi
+}
+
+find_rsync_path() {
+    local rsync_path=--rsync-path=$rsync_tilt_path
+    # see if we already have a compatible rsync on the container
+    local rsync_version=$(kubectl exec $K8S_OBJECT -- rsync --version 2> /dev/null \
+                              | head -1 | awk '{ print $3 }')
+    if [ "$rsync_version" ] && [[ "$rsync_version" > 3 ]]; then
+        rsync_path=
+    fi
+    echo $rsync_path
+}
 
 if [ -z "$KRSYNC_STARTED" ]; then
-  export K8S_OBJECT=$1
-  shift
-  export KRSYNC_STARTED=true
-  exec rsync --rsync-path=$rsync_path --blocking-io --rsh "$0" "$@"
+    # Quote/set to the entire first argument, which could contain
+    # '-n namespace'/'-c container' flags
+    export K8S_OBJECT="$1"
+    shift
+    export KRSYNC_STARTED=true
+    export RSYNC_PATH=$(find_rsync_path)
+    exec rsync $RSYNC_PATH --blocking-io --rsh "$0" "$@"
 fi
 
+## Shift away server and possible username arguments
 # Running as --rsh
-namespace_args=()
 depl=$1
 shift
 
@@ -27,19 +60,10 @@ shift
 if [ "X$depl" = "X-l" ]; then
     depl=$1
     shift
-    namespace_args=(-n "$1")
     shift
 fi
 
-# ensure our rsync bin is present on container in the expected location
-set +e # disable exit on error
-kubectl exec "$K8S_OBJECT" -- ls "$rsync_path" > /dev/null 2>&1
-exit_code="$?"
-set -e
-if [[ "$exit_code" != "0" ]]; then
-  # shellcheck disable=SC2002
-  cat "$tar_path_local" | kubectl "${namespace_args[@]}" exec -i "$K8S_OBJECT" -- tar -xf - -C /bin/
-fi
+install_rsync_tilt
 
-exec kubectl "${namespace_args[@]}" exec -i "$K8S_OBJECT" -- "$@"
-
+# $K8S_OBJECT is intentionally not quoted here, see above
+exec kubectl exec -i $K8S_OBJECT -- "$@"
