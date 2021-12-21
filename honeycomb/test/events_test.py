91836b1a5078590f8790a9dbd442527565054c34
Nick Santos <nick@tilt.dev>
2021-12-21 11:29:54 -0500

    honeycomb: make the test wait a bit on the apiserver

--- honeycomb/test/events_test.py
+++ honeycomb/test/events_test.py
@@ -1,9 +1,22 @@
 import json
 import subprocess
 import unittest
+import time
 
 class TestHoneycomb(unittest.TestCase):
   def test_events(self):
+    # Wait until the apiserver catches up.
+    wait_count = 0
+    while wait_count < 3:
+      events = json.loads(subprocess.check_output(["python3", "../events.py"]))
+      if (len(events) == 2 and
+          events[0]['data']['update_status'] == 'ok' and
+          events[1]['data']['update_status'] == 'in_progress'):
+        break
+
+      time.sleep(1)
+      wait_count = wait_count + 1
+
     events = json.loads(subprocess.check_output(["python3", "../events.py"]))
     self.assertEqual(2, len(events))
     self.assertEqual('(Tiltfile)', events[0]['data']['name'])
