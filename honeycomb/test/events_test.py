6f4d3436c557d70bb0810b0da1acb99c364120b6
Nick Santos <nick@tilt.dev>
2022-01-25 16:12:09 -0500

    honeycomb: support custom_build and add tests" (#329)
    * honeycomb: support custom_build
    
    * add try catch so there are no errors on old Tilt versions
    
    * fix typo
    
    * Don't require python 3.9
    
    * honeycomb: add tests
    
    Co-authored-by: Dan Miller <dan@replay.io>

--- honeycomb/test/events_test.py
+++ honeycomb/test/events_test.py
@@ -9,20 +9,25 @@ class TestHoneycomb(unittest.TestCase):
     wait_count = 0
     while wait_count < 3:
       events = json.loads(subprocess.check_output(["python3", "../events.py"]))
-      if (len(events) == 2 and
+      if (len(events) == 6 and
           events[0]['data']['update_status'] == 'ok' and
-          events[1]['data']['update_status'] == 'in_progress'):
+          events[2]['data']['update_status'] == 'in_progress'):
         break
 
       time.sleep(1)
       wait_count = wait_count + 1
 
     events = json.loads(subprocess.check_output(["python3", "../events.py"]))
-    self.assertEqual(2, len(events))
-    self.assertEqual('(Tiltfile)', events[0]['data']['name'])
-    self.assertEqual('ok', events[0]['data']['update_status'])
-    self.assertEqual('event-test', events[1]['data']['name'])
-    self.assertEqual('in_progress', events[1]['data']['update_status'])
+    self.assertEqual(6, len(events))
+
+    self.assertEqual(['uiresource', 'uiresource', 'uiresource', 'uiresource', 'dockerimage', 'cmdimage'],
+                     [e['data']['kind'] for e in events])
+
+    res_events = [e for e in events if e['data']['kind'] == 'uiresource']
+    self.assertEqual(['(Tiltfile)', 'bobo', 'event-test', 'uncategorized'],
+                     [e['data']['name'] for e in res_events])
+    self.assertEqual(['ok', 'ok', 'in_progress', 'ok'],
+                     [e['data']['update_status'] for e in res_events])
 
 if __name__ == '__main__':
     unittest.main()
