f2bef0149189523d44ceaaee826a013199f8b269
Nick Santos <nick@tilt.dev>
2022-04-06 18:57:53 -0400

    test: disable pulumi test if no pulumi user (#378)

--- pulumi/test/test.sh
+++ pulumi/test/test.sh
@@ -4,6 +4,15 @@ cd "$(dirname "$0")"
 
 set -ex
 
+# Disable the pulumi test if no pulumi user.
+# Circleci won't inject our pulumi secrets for PRs, but
+# we still want to run other integration tests.
+PULUMI_USER=$(pulumi whoami --non-interactive)
+if [[ "$PULUMI_USER" == "" ]]; then
+    echo "No pulumi user. Skipping pulumi test"
+    exit 0
+fi
+
 # install pulumi deps
 yarn install
 
