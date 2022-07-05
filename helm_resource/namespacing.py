4d8783865f4e713e4fd218af9e623b1312cf21a1
Nick Santos <nick.santos@docker.com>
2022-07-05 16:02:53 -0400

    helm_resource: improve namespace defaulting. fixes https://github.com/tilt-dev/tilt-extensions/issues/419 (#421)

---
+++ helm_resource/namespacing.py
@@ -0,0 +1,66 @@
+# Helper functions for defaulting yaml.
+
+import os
+import re
+import subprocess
+import sys
+from typing import Dict
+
+# Add default namespace for a single resource.
+def add_default_namespace_resource(r, namespace, indent=""):
+  # Find the part of the yaml that has the metadata: and following indented lines.
+  meta = re.search("^%smetadata:\n(\\s+.*\n)*" % indent, r, re.MULTILINE)
+  if not meta:
+    return r
+
+  metadata = meta.group(0)
+
+  # Remove empty namespace blocks.
+  metadata = re.sub("\n\\s+namespace:\\s*\n", "\n", metadata, flags=re.MULTILINE)
+
+  has_namespace = re.search("\n\\s+namespace: *\\S", metadata, re.MULTILINE)
+  if not has_namespace:
+    metadata = re.sub("^%smetadata:" % indent,
+                      "%smetadata:\n%s  namespace: %s" % (indent, indent, namespace),
+                      metadata, 1)
+    return r[0:meta.start()] + metadata + r[meta.end():]
+
+  return r
+
+# Add default namespace for a v1.List by half-assedly
+# splitting the List into items.
+def add_default_namespace_resource_list(r, namespace):
+  meta = re.search("^(\\s+)metadata:\\s*$", r, flags=re.MULTILINE)
+  if not meta:
+    return r
+
+  indent = meta.group(1)
+  items = re.split('^%smetadata:\\s*$' % indent, r, flags=re.MULTILINE)
+  for i in range(len(items)):
+    if i == 0:
+      continue
+
+    items[i] = add_default_namespace_resource(
+      '%smetadata:%s' % (indent, items[i]), namespace, indent=indent)
+
+  return ''.join(items)
+
+# We have to do namespace defaulting ourselves :(
+# See: https://github.com/tilt-dev/tilt-extensions/issues/374
+def add_default_namespace(yaml, namespace):
+  if not namespace:
+    return yaml
+
+  resources = re.split('^---$', yaml, flags=re.MULTILINE)
+
+  for i in range(len(resources)):
+    r = resources[i]
+
+    # Check to see if this is a resource list.
+    kind = re.search("^kind:\\s*(\\w+)\\s*$", r, flags=re.MULTILINE)
+    if kind and (kind.group(1) == "List"):
+      resources[i] = add_default_namespace_resource_list(r, namespace)
+    else:
+      resources[i] = add_default_namespace_resource(r, namespace)
+
+  return '---'.join(resources)
