396839f9d70930beb8a6425595c9f01459110fac
Tristan Ross <tristan.ross@midstall.com>
2022-07-31 11:00:40 -0700

    feat(pulumi): helm resources are somewhat tracked
    This commit introduces a patch to the "pulumi-get.py" script.
    With this patch, basic Kubernetes resources are tracked.
    This allows for Tilt to properly track these resources.
    
    Signed-off-by: Tristan Ross <tristan.ross@midstall.com>

--- pulumi/pulumi-get.py
+++ pulumi/pulumi-get.py
@@ -7,6 +7,25 @@ import json
 import subprocess
 import sys
 
+helmResourceKindMap = {
+  'ConfigMap/v1': 'configmaps',
+  'Deployment.apps/apps/v1': 'deployments',
+  'Secret/v1': 'secrets',
+  'Service/v1': 'services',
+  'ServiceAccount/v1': 'serviceaccounts',
+  'StatefulSets.apps/apps/v1': 'statefulsets',
+}
+
+def dumpResource(ns, kind, name):
+  if kind and name:
+    args = ['kubectl', 'get']
+    if ns:
+      args.extend(['-n', ns])
+    
+    args.extend([kind, name, '-o=yaml'])
+    print(subprocess.check_output(args).decode('utf-8'))
+    print('---')
+
 export_cmd = ['pulumi', 'stack', 'export']
 if len(sys.argv) >= 2:
   export_cmd.extend(['-s', sys.argv[1]])
@@ -20,14 +39,26 @@ for resource in resources:
   if not is_k8s:
     continue
 
-  if not t.find('kubernetes:helm.sh/') == 0:
+  if t.find('kubernetes:helm.sh/') == 0:
+    if t == 'kubernetes:helm.sh/v3:Release':
+      helmResources = resource.get('resourceNames', {})
+      for helmResourceKind in helmResources:
+        if helmResourceKind in helmResourceKindMap:
+          kind = helmResourceKindMap[helmResourceKind]
+          for helmResource in helmResourceKind:
+            ns = isinstance(resource.get('namespace', '')) and resource.get('namespace', '')
+            name = helmResource
+            if helmResource.find('/') > 0:
+              ns = helmResource[0:helmResource.find('/')]
+              name = helmResource[helmResource.find('/') + 1:]
+            
+            dumpResource(ns, kind, name)
+  else:
     o = resource.get('outputs', {})
     if not o.get('kind', ''):
       continue
 
     kind = o.get('kind', '').lower()
     name = o.get('metadata', {}).get('name', '')
-    namespace = o.get('metadata', {}).get('namespace', '')
-    if kind and name:
-      print(subprocess.check_output(['kubectl', 'get', '-n', namespace, kind, name, '-o=yaml']).decode('utf-8'))
-      print('---')
+    ns = o.get('metadata', {}).get('namespace', '')
+    dumpResource(ns, kind, name)
