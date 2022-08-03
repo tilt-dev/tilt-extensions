8d3b30fdf0d07689f69dd0d4c4f7e1e94a74e779
Tristan Ross <tristan.ross@midstall.com>
2022-08-03 11:43:45 -0700

    Track basic Kubernetes resources in Pulumi from Helm (#430)
    * fix(pulumi): #423 check with namespaces
    
    Signed-off-by: Tristan Ross <tristan.ross@midstall.com>
    
    * feat(pulumi): skip over resources which cannot be fetched with kubectl
    
    Signed-off-by: Tristan Ross <tristan.ross@midstall.com>
    
    * Expose disabled and requires_confirmation for uibutton extension (#428)
    
    * Expose disabled and requires_confirmation for uibutton extension
    
    Signed-off-by: MarcDufresne <marc-andre.dufresne@novisto.com>
    
    * Update tests
    
    Signed-off-by: MarcDufresne <marc-andre.dufresne@novisto.com>
    Signed-off-by: Tristan Ross <tristan.ross@midstall.com>
    
    * feat(pulumi): helm resources are somewhat tracked
    
    This commit introduces a patch to the "pulumi-get.py" script.
    With this patch, basic Kubernetes resources are tracked.
    This allows for Tilt to properly track these resources.
    
    Signed-off-by: Tristan Ross <tristan.ross@midstall.com>
    
    * fix(pulumi): actually work with helm
    
    Last commit had a few bugs, this fixes those bugs
    so everything is working right now. :)
    
    Signed-off-by: Tristan Ross <tristan.ross@midstall.com>
    
    Co-authored-by: Marc-André Dufresne <madmoi23@gmail.com>

--- pulumi/pulumi-get.py
+++ pulumi/pulumi-get.py
@@ -7,6 +7,25 @@ import json
 import subprocess
 import sys
 
+helmResourceKindMap = {
+  'ConfigMap/v1': 'configmaps',
+  'Deployment.apps/apps/v1': 'deployments',
+  'Secret/v1': 'secrets',
+  'Service/v1': 'services',
+  'ServiceAccount/v1': 'serviceaccounts',
+  'StatefulSets.apps/apps/v1': 'statefulsets',
+}
+
+def dumpResource(ns, kind, name):
+  if kind and name:
+    args = ['kubectl', 'get']
+    if ns:
+      args.extend(['-n', ns])
+    
+    args.extend([kind, name, '-o=yaml'])
+    print(subprocess.check_output(args).decode('utf-8'))
+    print('---')
+
 export_cmd = ['pulumi', 'stack', 'export']
 if len(sys.argv) >= 2:
   export_cmd.extend(['-s', sys.argv[1]])
@@ -19,15 +38,28 @@ for resource in resources:
   is_k8s = isinstance(t, str) and t.find('kubernetes:') == 0
   if not is_k8s:
     continue
+  
+  o = resource.get('outputs', {})
 
-  if not t.find('kubernetes:helm.sh/') == 0:
-    o = resource.get('outputs', {})
+  if t.find('kubernetes:helm.sh/') == 0:
+    if t == 'kubernetes:helm.sh/v3:Release':
+      helmResources = o.get('resourceNames', {})
+      for helmResourceKind in helmResources:
+        if helmResourceKind in helmResourceKindMap:
+          kind = helmResourceKindMap[helmResourceKind]
+          for helmResource in helmResources[helmResourceKind]:
+            ns = isinstance(o.get('namespace', ''), str) and o.get('namespace', '')
+            name = helmResource
+            if helmResource.find('/') > 0:
+              ns = helmResource[0:helmResource.find('/')]
+              name = helmResource[helmResource.find('/') + 1:len(helmResource)]
+            
+            dumpResource(ns, kind, name)
+  else:
     if not o.get('kind', ''):
       continue
 
     kind = o.get('kind', '').lower()
     name = o.get('metadata', {}).get('name', '')
-    namespace = o.get('metadata', {}).get('namespace', '')
-    if kind and name:
-      print(subprocess.check_output(['kubectl', 'get', '-n', namespace, kind, name, '-o=yaml']).decode('utf-8'))
-      print('---')
+    ns = o.get('metadata', {}).get('namespace', '')
+    dumpResource(ns, kind, name)
