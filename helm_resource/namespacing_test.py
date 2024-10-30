117769f264db212371ebe9c095ff81e13e4fb541
Nick Santos <nick.santos@docker.com>
2024-10-30 09:49:00 -0400

    helm_resource: correctly handle DOS line endings
    fixes https://github.com/tilt-dev/tilt-extensions/issues/607
    
    Signed-off-by: Nick Santos <nick.santos@docker.com>

--- helm_resource/namespacing_test.py
+++ helm_resource/namespacing_test.py
@@ -1,9 +1,15 @@
+import re
 import unittest
 from namespacing import add_default_namespace
 
 class TestNamespacing(unittest.TestCase):
   def assert_defaulted(self, expected, original, ns):
-    self.assertEqual(expected, add_default_namespace(original, ns))
+    actual = add_default_namespace(original, ns)
+
+    # Normalize line endings.
+    actual_norm = re.sub("\r\n", "\n", actual)
+
+    self.assertEqual(expected, actual_norm)
 
   def test_add_when_omitted(self):
     self.assert_defaulted("""
@@ -19,6 +25,22 @@ metadata:
   name: hello
 """, "my-namespace")
 
+  def test_add_when_omitted_dos(self):
+    self.assert_defaulted("""
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: my-namespace
+  name: hello
+""", "\r\n".join([
+    "",
+    "apiVersion: v1",
+    "kind: ConfigMap",
+    "metadata:",
+    "  name: hello",
+    ""
+    ]), "my-namespace")
+
   def test_add_when_empty(self):
     self.assert_defaulted("""
 apiVersion: v1
@@ -33,6 +55,24 @@ metadata:
   name: hello
   namespace:
 """, "my-namespace")
+
+  def test_add_when_empty_dos(self):
+    self.assert_defaulted("""
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: my-namespace
+  name: hello
+""",
+        "\r\n".join([
+            "",
+            "apiVersion: v1",
+            "kind: ConfigMap",
+            "metadata:",
+            "  name: hello",
+            "  namespace:",
+            ""
+        ]), "my-namespace")
 
   def test_add_in_list(self):
     self.assert_defaulted("""
@@ -77,6 +117,23 @@ metadata:
   name: hello
   namespace: world
 """, "my-namespace")
+
+  def test_ignore_explicit_dos(self):
+    self.assert_defaulted("""
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: hello
+  namespace: world
+""", "\r\n".join([
+      "",
+      "apiVersion: v1",
+      "kind: ConfigMap",
+      "metadata:",
+      "  name: hello",
+      "  namespace: world",
+      ""
+    ]), "my-namespace")
 
 
 if __name__ == '__main__':
