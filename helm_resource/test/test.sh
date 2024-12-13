14036e70a4bb18efa703996cf9ec002f641cd702
Nick Santos <nick.santos@docker.com>
2024-12-12 22:58:13 -0500

    WIP: repro steps
    https://github.com/tilt-dev/tilt-extensions/issues/599
    Signed-off-by: Nick Santos <nick.santos@docker.com>

--- helm_resource/test/test.sh
+++ helm_resource/test/test.sh
@@ -4,5 +4,9 @@ set -ex
 
 cd "$(dirname "$0")"
 
-tilt ci
-tilt down
+kubectl config set-cluster dummy --server=http://localhost:1234
+kubectl config set-context dummy --cluster=dummy
+kubectl config use-context dummy
+
+tilt ci --context=kind-kind
+tilt down --context=kind-kind
