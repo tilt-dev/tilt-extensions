d85e3b442067cebb99be34f313bb9e99a67d3e83
arnas dundulis <arnas@vinted.com>
2026-07-20 16:11:01 +0300

    fix timeout failing to call fg
    Signed-off-by: arnas dundulis <arnas@vinted.com>

--- cancel/test/test.sh
+++ cancel/test/test.sh
@@ -12,11 +12,15 @@ timeout 30 tail -f tilt.log | grep -q "cmd.tilt.dev/sleep:update:cancel created"
 # Hit the cancel button. This should make tilt exit.
 ./trigger.sh
 
-# Move tilt ci to the foreground, and ignore errors
-timeout 15 fg
-
-grep -q "Running cmd: ./kill_cmd.sh sleep:update" tilt.log
-RESULT=$?
+# Wait for the asynchronous button event to run the cancellation command.
+RESULT=1
+for _ in {1..300}; do
+  if grep -q "Running cmd: ./kill_cmd.sh sleep:update" tilt.log; then
+    RESULT=0
+    break
+  fi
+  sleep 0.1
+done
 
 cat tilt.log
 rm tilt.log
