3a1947b19641729c0b26f95d2bf6bd11c1833498
arnas dundulis <arnas@vinted.com>
2026-07-20 16:17:50 +0300

    fix request missing session token
    Signed-off-by: arnas dundulis <arnas@vinted.com>

--- cancel/test/trigger.sh
+++ cancel/test/trigger.sh
@@ -10,5 +10,9 @@ NEW_YAML=$(echo "$YAML" | sed "s/lastClickedAt.*/lastClickedAt: $TIME/g")
 # Follow this KEP:
 # https://github.com/kubernetes/enhancements/issues/2590
 # For now, we can handle it with curl.
-curl -X PUT -H "Content-Type: application/yaml" -d "$NEW_YAML" \
+COOKIE_JAR=$(mktemp)
+trap 'rm -f "$COOKIE_JAR"' EXIT
+curl -fsS -c "$COOKIE_JAR" http://localhost:10350/ >/dev/null
+curl -fsS -b "$COOKIE_JAR" \
+  -X PUT -H "Content-Type: application/yaml" -d "$NEW_YAML" \
    http://localhost:10350/proxy/apis/tilt.dev/v1alpha1/uibuttons/sleep:update:cancel/status
